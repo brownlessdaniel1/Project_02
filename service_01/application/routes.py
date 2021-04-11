ce3ab23ce06dd1cf1c91ddcfa1af6690acaa26c6
brownlessdaniel1 <DBrownless@qa.com>
2021-04-11 12:25:52 +0000

    db: test displaying json output #9

--- service_01/application/routes.py
+++ service_01/application/routes.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, Response, jsonify
 import requests
 import os
-import json
 from application import app
 
 service_2 = "http://service_02:5001"
@@ -17,11 +16,10 @@ def home():
     ints_to_be_sent = {"int_one":response_service_2, "int_two":response_service_3}
     response_service_4 = requests.post(service_4 + "/sum_ints", json=ints_to_be_sent).json()["output"]
 
-
     data_to_be_saved = {"int_one":response_service_2, "int_two":response_service_3, "sum":response_service_4} 
 
     previous_records = requests.post(service_4 + "/record_data", json=data_to_be_saved).json()
 
-    # data = json.loads(previous_records)
+    data = str(previous_records)
 
-    return render_template("index.html", int_one=str(response_service_2), int_two=str(response_service_3), total= response_service_4, previous_records=previous_records, version=os.getenv("VERSION"))
+    return render_template("index.html", int_one=str(response_service_2), int_two=str(response_service_3), total= response_service_4, previous_records=data, version=os.getenv("VERSION"))
