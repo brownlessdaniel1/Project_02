0780ccfb44f8d90e05a7143d74c687733d96db26
brownlessdaniel1 <DBrownless@qa.com>
2021-04-11 07:44:57 +0000

    db: basic testing.sh

--- testing.sh
+++ testing.sh
@@ -1,1 +1,31 @@
-echo "testinggg"
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python3-venv
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r test-requirements.txt
+echo "done!"
+
+# test service 1
+cd service_01
+pytest
+cd ..
+# test service 2
+cd service_02
+pytest
+cd ..
+# test service 3
+cd service_03
+pytest
+cd ..
+# test service 4
+cd service_04
+pytest
+cd ..
+
+deactivate
+rm -r .venv
+echo "finished!"
