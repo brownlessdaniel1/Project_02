9497982f70a4702f06fc1297f0b0f62e139961b8
brownlessdaniel1 <DBrownless@qa.com>
2021-04-12 08:35:44 +0000

    db: Mock Rollback

--- service_03/application/routes.py
+++ service_03/application/routes.py
@@ -7,7 +7,7 @@ def colour_gen():
 
     # return a randomly selected colour
 
-    colours = ["red"]
+    colours = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "purple", "pink", "silver", "gold", "beige", "brown", "grey", "black", "white"]
     output = choice(colours)
 
     return jsonify({"output": output})
